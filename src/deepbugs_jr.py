77d26476de27acb68778b348ac732fa37aa4d738
Dario-Ugalde <38869022+Dario-Ugalde@users.noreply.github.com>
2021-03-04 11:46:25 -0600

    Test file must be specified if the file doesn't begin with the word test

--- src/deepbugs_jr.py
+++ src/deepbugs_jr.py
@@ -1,8 +1,12 @@
-def main():
-    print("Hello World!")
-
 def test(x):
     return x
+
+def main():
+    values = [3.14, 0, "Hello World", True]
+    message = "Given {parameter} the function returns {value}."
+    for x in values:
+        data = test(x)
+        print(message.format(parameter=x, value=data))
 
 if __name__ == "__main__":
     main()
