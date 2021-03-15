dbd473818a413835f6ae8af0b8405902dfe9879c
jmw150 <jmw150@hotmail.com>
2021-03-15 16:17:19 -0400

    done implementation. Needs tests

--- gen_train_eval.py
+++ gen_train_eval.py
@@ -1,18 +1,43 @@
+# part 3
+
+import os
+from random import random
+
 from ast_token_extractor import *
+from swarg_fnargs2tokens import get_all_2_arg_fn_calls_from_file as get_funs
 
-# stub/interface set
-Word2Vec = lambda x : 1
-Token = lambda x : ast2id_or_lit(x)
+positiveExample_p = 0.8
 
-fnName = 0
-arg1 = 1
-arg2 = 2
+data_path = "data/ast_for_protoyping/"
+lamdba word2vec = x : Word2Vec(x)
 
+#Question: are these vectors simply additive?
+def gen_good_bad_fun_args (data_path) :
+    """make good bad function args
 
-# would be simpler to pull from the function block
-positiveExample = #lambda fnName : 
-    Word2Vec(Token(fnName)) + Word2Vec(Token(arg1)) + Word2Vec(Token(arg2))
-negativeExample = #lambda fnName : 
-    Word2Vec(Token(fnName)) + Word2Vec(Token(arg2)) + Word2Vec(Token(arg1))
+    Args:
+        ast_foldername (str): filepath to the AST jsons 
 
-
+    Returns:
+        tuple[list[vectors]]: A tuple of (good, bad) list of function vectors
+    """
+    
+    positiveExample = []
+    negativeExample = []
+    for File in os.listdir(data_path) :
+        some_fn_calls = get_funs(File)
+        
+        for fn_call in some_fn_calls :
+            if random() > positiveExample_p :
+                positiveExample.append(
+                    word2vec(fn_call["fn_name"]) +
+                    word2vec(fn_call["arg1"]) +
+                    word2vec(fn_call["arg2"]))
+            else :
+                negativeExample.append(
+                    word2vec(fn_call["fn_name"]) +
+                    word2vec(fn_call["arg2"]) +
+                    word2vec(fn_call["arg1"]))
+    
+        return (positiveExample, negativeExample)
+         
