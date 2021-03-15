a85a1e200967006c3649d196e1a0dcce09887f7e
Dario Ugalde <dario.ugalde@outlook.com>
2021-03-15 17:58:02 -0500

    Implementing usage of ast tokens and creating model

--- deepbugs_jr.py
+++ deepbugs_jr.py
@@ -1,12 +1,49 @@
-def test(x):
-    return x
+from ast_token_extractor import ast2id_or_lit
+from gensim.models import Word2Vec
+import json
+import time
+import math
+import sys
+
+# from HyperParameters import name_embedding_size
+
+def convert_to_ast(lines):
+    file_str = ''
+        
+    for l in lines:
+        file_str += l
+    file_str.replace('\n', '')
+    nodes = eval(file_str)
+
+    tokens = []
+    for node in nodes:
+        tokens.append(ast2id_or_lit(node))
+
+    return tokens
 
 def main():
-    values = [3.14, 0, "Hello World", True]
-    message = "Given {parameter} the function returns {value}."
-    for x in values:
-        data = test(x)
-        print(message.format(parameter=x, value=data))
+    tokens = [] 
+    for i in range(10):
+        File = open(f"data/ast_for_prototyping/ast_{i}.json",'r')
+        lines = File.readlines()
+        File.close()
+        tokens.append(convert_to_ast(lines))
+
+    model = Word2Vec(tokens, min_count=3, window=5, size=150, workers=40, iter=15, alpha=0.1, sg=0)
+
+    timestamp = math.floor(time.time() * 1000)
+    model.save("embedding_model_" + str(timestamp))
+
+    token_to_vector = dict()
+    for token in model.wv.vocab:
+        if token.startswith("ID:") or token.startswith("LIT:"):
+            vector = model[token].tolist
+            token_to_vector[token] = vector
+    token_to_vector_file_name = "token_to_vector_" + str(timestamp) + ".json"
+    with open(token_to_vector_file_name, "w") as file:
+        json.dump(token_to_vector, file, sort_keys=True, indent=4)
+
+
 
 if __name__ == "__main__":
     main()
